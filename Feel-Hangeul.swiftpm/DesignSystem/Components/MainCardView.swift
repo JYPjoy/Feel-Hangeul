64f8f0a36731a503ba5ebbf2b64fd3ec1cf252d1
JOY <jypjoy@gmail.com>
2023-04-18 01:17:11 +0900

    ✨ feat: MainView 색상 변경

--- Feel-Hangeul.swiftpm/DesignSystem/Components/MainCardView.swift
+++ Feel-Hangeul.swiftpm/DesignSystem/Components/MainCardView.swift
@@ -18,7 +18,7 @@ var wordCard = [
     wordCardView(word: "주룩주룩", pronunciation: "[Ju-Ruk-Ju-Ruk]",color: .red, destination: .jurukjuruk),
     wordCardView(word: "쨍쨍", pronunciation: "[Jjaeng-Jjaeng]", color: .orange, destination: .jjaengjjaeng),
     wordCardView(word: "출렁출렁", pronunciation: "[Chul-Leong-Chul-Leong]", color: .yellow, destination: .chulleongchulleong),
-    wordCardView(word: "두근두근", pronunciation: "[Du-Geun-Du-Geun]", color: .blue, destination: .dugeundugeun),
-    wordCardView(word: "반짝반짝",pronunciation: "[Ju-Ruk-Ju-Ruk]", color: .green, destination: .jurukjuruk),
-    wordCardView(word: "생글생글", pronunciation: "[Ju-Ruk-Ju-Ruk]", color: .gray, destination: .jurukjuruk)
+    wordCardView(word: "두근두근", pronunciation: "[Du-Geun-Du-Geun]", color: .green, destination: .dugeundugeun),
+    wordCardView(word: "반짝반짝",pronunciation: "[Ju-Ruk-Ju-Ruk]", color: .blue, destination: .jurukjuruk),
+    wordCardView(word: "생글생글", pronunciation: "[Ju-Ruk-Ju-Ruk]", color: .purple, destination: .jurukjuruk)
 ]
