0e7b490352beb2de6b0c58007850a9536fc41bdd
JOY <jypjoy@gmail.com>
2023-04-18 13:21:11 +0900

    ✨ feat: 방울방울 효과 추가

--- Feel-Hangeul.swiftpm/Presentation/WordScene/BangUlBangUlView.swift
+++ Feel-Hangeul.swiftpm/Presentation/WordScene/BangUlBangUlView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct BangUlBangUlView: View {
+    @State private var bubble = false
+    
+    
     var body: some View {
         ZStack{
             Color.black
                 .ignoresSafeArea()
             
-            Text("Hello, world")
+            Text("방울")
+                .font(.system(size: 100))
                 .foregroundColor(.white)
+                .overlay{
+                    Circle()
+                        .frame(width: 200, height:200)
+                        .opacity(0.5)
+                        .foregroundColor(bubble ? .yellow : .clear)
+                }
+                .onTapGesture {
+                    bubble.toggle()
+                }
         }
     }
 }
+
+
+//TODO: 할 일
+/*
+- 색깔 바뀔 때 애니메이션 효과 추가하기
+- 방울방울 글씨를 여기저기에 뿌리기 (Bubble처럼)
+- TapGesture 시 bubble이 나타나는 효과 추가하기
+
+ */
+
+
+
+
 
 struct BangUlBangUlView_Previews: PreviewProvider {
     static var previews: some View {
